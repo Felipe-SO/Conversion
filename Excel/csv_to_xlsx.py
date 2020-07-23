07d76eb7141540f19f28ad2e5a9246f09d19095a
Felipe-SO <68606807+Felipe-SO@users.noreply.github.com>
2020-07-23 18:18:31 -0500

    Add files via upload

---
+++ Excel/csv_to_xlsx.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+# Converts .xlsx to .csv.
+# Insert the path (as a string) to the exact folder where your files are.
+def csv_xlsx(path):
+    for file in os.listdir(path):
+        lencsv=len(file)
+        if file.endswith(".csv")==True:
+            if "%s.xlsx"%(file[0:lencsv-4]) in os.listdir(path):
+                print("%s.xlsx already exists." %(file[0:lencsv-4]))
+            else:
+                os.chdir(path)
+                Data = pd.read_csv(file,index_col=0)
+                Data.to_excel("%s.xlsx"%(file[0:lencsv-4]))
+                print("Created %s.xlsx from %s." % (file[0:lencsv-4],file))
+            
